bcc76ed6aa3926c677479912a954e913a5703c01
Yuto Otaguro <yotagur@gmail.com>
2019-08-13 20:57:39 -0500

    Create alternate route for data drill

--- web/routes/user.py
+++ web/routes/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from ..db import get_lurker_collection
-
+from bson.json_util import dumps
 
 user_bp = Blueprint(
     'user', __name__, template_folder='templates', url_prefix='/user')
@@ -14,7 +14,42 @@ def get_user_graph(user_id):
     times = coll.game_time.find({'userId': user_id})
     games = times.distinct('gameName')
     games.sort()
-    
+
+    times = coll.game_time.aggregate([
+        {
+            '$match': {
+                'userId': '111703434985529344'
+            }},
+        {
+            '$group': {
+                '_id': "$gameName",
+                'time': {'$sum': {
+                    '$divide': [{
+                        '$subtract': [
+                            "$sessionEnd", "$sessionBegin"
+                        ]}, 1000
+                    ]}
+                }
+            }
+        },
+        {
+            '$sort': {
+                "_id": 1
+            }
+        }])
+
+    return render_template('user-time.html', user_info=user, games=games, times=dumps(times))
+
+
+@user_bp.route('/<user_id>/<game_name>')
+def get_user_game_graph(user_id):
+    coll = get_lurker_collection()
+
+    user = coll.discord_db_user.find_one({'userId': user_id})
+    times = coll.game_time.find({'userId': user_id})
+    games = times.distinct('gameName')
+    games.sort()
+
     times = coll.game_time.aggregate([
         {
             '$match': {
