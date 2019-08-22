0ffb12bb89cbbc3f920b11a6b8e76c51bc2cf2d7
Yuto Otaguro <yotagur@gmail.com>
2019-08-21 21:32:51 -0500

    Live feed properly mounts onto the page

---
+++ web/static/js/components/live-feed/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class LiveFeedComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                TEST
+            </div>
+        )
+    }
+}
+
+export default LiveFeedComponent;
--- web/static/js/user/live-feed.tsx
+++ web/static/js/user/live-feed.tsx
@@ -1,2 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+
+import Component from '../components/live-feed'
+
+render(<Component />, document.getElementById('live-feed-entry'))
