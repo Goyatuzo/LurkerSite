84287189ff4f8cde71ee8b7ff8c98ff305809c45
Yuto Otaguro <yotagur@gmail.com>
2019-08-21 20:17:36 -0500

    API returns feed of 15 recently played games

--- web/api/game_time.py
+++ web/api/game_time.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request, Response
+from bson.json_util import dumps
 import re
 from ..db import get_game_times
 
@@ -9,7 +10,39 @@ api_game_time_bp = Blueprint(
 @api_game_time_bp.route('/feed/<user_id>', methods=['GET'])
 def get_feed(user_id: str):
     """Get a feed of the past 5 games played."""
+    game_times_coll = get_game_times()
 
-    game_times = get_game_times()
+    times = game_times_coll.aggregate([
+        {
+            '$match': {
+                'userId': user_id
+            }
+        },
+        {
+            '$sort': {
+                'sessionEnd': -1
+            }
+        },
+        {
+            '$limit': 15
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'gameName': '$gameName',
+                'gameDetail': '$gameDetail',
+                'gameState': '$gameState',
+                'time': {'$sum': {
+                    '$divide': [{
+                        '$subtract': [
+                            "$sessionEnd", "$sessionBegin"
+                        ]}, 3600000
+                    ]}
+                }
+            }
+        }
+    ])
 
-    return jsonify({})
+    stored = list(times)
+
+    return jsonify(stored)
