42c7380b6470ef5fd9bdf5a746cbb47680cde6b8
Yuto Otaguro <yotagur@gmail.com>
2019-08-25 12:35:49 -0500

    Create tests for rendering / time checking live feed

--- js/components/__test__/live-feed/item.test.tsx
+++ js/components/__test__/live-feed/item.test.tsx
@@ -1,15 +1,38 @@
 import * as React from 'React';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, ShallowWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import addSeconds from 'date-fns/addSeconds'
 
 import { gameFeedOne } from '../data/game-feed';
 import LiveFeedItem from '../../live-feed/item';
 
 configure({ adapter: new Adapter() });
 describe(`Live Feed Item test`, () => {
-    it('League of Legends text appears', () => {
-        const comp = shallow(<LiveFeedItem feedEntry={gameFeedOne} />)
+    let comp: ShallowWrapper;
+    const now = new Date(gameFeedOne.sessionEnd);
 
+    beforeEach(() => {
+        comp = shallow(<LiveFeedItem feedEntry={gameFeedOne} currentTime={now} />)
+    });
+
+    it('League of Legends text appears', () => {
         expect(comp.contains("League of Legends")).toBeTruthy();
-    })
+    });
+
+    describe("time span test", () => {
+        it('0 seconds ago', () => {
+            const currentTime = gameFeedOne.sessionEnd;
+
+            expect(comp.contains("0 seconds ago")).toBeTruthy()
+        });
+
+        it('1 seconds ago', () => {
+            const now = addSeconds(new Date(gameFeedOne.sessionEnd), 1);
+            comp.setProps({
+                currentTime: now
+            });
+
+            expect(comp.contains("1 seconds ago")).toBeTruthy()
+        });
+    });
 });
