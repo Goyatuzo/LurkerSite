64d3b39ff248939afa6e4592f0255b376c89b1db
Yuto Otaguro <yotagur@gmail.com>
2019-08-28 21:15:58 -0500

    MongoDB returns the count every hour

--- web/api/stats/line_graph.py
+++ web/api/stats/line_graph.py
@@ -1,3 +1,5 @@
+import pytz
+
 from datetime import datetime, timedelta
 
 from ...db import get_game_times
@@ -6,28 +8,47 @@ from ...db import get_game_times
 def get_stats_line_graph():
     game_times = get_game_times()
 
-    time_query = game_times.aggregate([
-        {
-            '$match': {
-                'sessionEnd': {'$gte': datetime.now() - timedelta(days=2)}
-            }
-        },
-        {
-            '$sort': {
-                'sessionEnd': -1
-            }
-        },
-        {
-            '$project': {
-                '_id': 0
-            }
-        }
-    ])
+    limit = datetime.now().astimezone(pytz.utc) - timedelta(days=2)
+    curr = datetime.now().astimezone(pytz.utc).replace(
+        minute=0, second=0, microsecond=0)
 
-    line_graph = list(time_query)
+    time_count = {}
 
-    for entry in line_graph:
-        entry['sessionBegin'] = entry['sessionBegin'].isoformat() + '+00:00'
-        entry['sessionEnd'] = entry['sessionEnd'].isoformat() + '+00:00'
+    while curr > limit:
+        time_query = game_times.aggregate([
+            {
+                '$match': {
+                    'sessionEnd': {'$gte': datetime.now() - timedelta(days=2)}
+                }
+            },
+            {
+                '$sort': {
+                    'sessionEnd': -1
+                }
+            },
+            {
+                '$group': {
+                    '_id': '$userId',
+                }
+            },
+            {
+                '$group': {
+                    '_id': 1,
+                    'count': {
+                        '$sum': 1
+                    }
+                }
+            },
+            {
+                '$project': {
+                    '_id': 0
+                }
+            }
+        ])
 
-    return line_graph
+        line_graph = list(time_query)
+
+        time_count[curr.isoformat()] = line_graph
+        curr = curr - timedelta(hours=1)
+
+    return time_count
