28a9b02a28af8f9bb9ddfcd44ac17ea33aa57f5f
Yuto Otaguro <yotagur@gmail.com>
2019-09-01 13:23:30 -0500

    Test number of players component's immutability

--- js/components/__test__/stats/number-of-players.test.tsx
+++ js/components/__test__/stats/number-of-players.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { shallow, configure, ShallowWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import parseISO from 'date-fns/parseISO';
 
 import { playedHours } from '../data/played-hours';
 import NumberOfPlayers, { NOPProps, NOPState } from '../../stats/number-of-players';
@@ -11,17 +10,34 @@ describe(`Number of Players (Line Graph)`, () => {
     let comp: ShallowWrapper<NOPProps, NOPState>;
     const time = new Date(playedHours[0].t);
 
-    beforeAll(() => {
-        comp = shallow(<NumberOfPlayers chartId="playah-id" entries={playedHours} firstHour={time} />)
-    });
-
     describe('state', () => {
+        beforeAll(() => {
+            comp = shallow(<NumberOfPlayers chartId="playah-id" entries={playedHours} firstHour={time} />)
+        });
+
         it('default test case shoul have 4 points', () => {
             expect(comp.state().timePoints).toHaveLength(4);
         });
 
         it('dataLoaded should be set to true', () => {
             expect(comp.state().dataLoaded).toBeTruthy();
-        })
+        });
+
+        it('updating the props should do nothing', () => {
+            comp.setProps({ entries: [] });
+            comp.update();
+
+            expect(comp.state().timePoints).toHaveLength(4);
+        });
+    });
+
+    describe('first hour is latest', () => {
+        beforeAll(() => {
+            comp = shallow(<NumberOfPlayers chartId="playah-id" entries={playedHours} firstHour={new Date(playedHours[playedHours.length - 1].t)} />)
+        });
+
+        it('should only have one time point', () => {
+            expect(comp.state().timePoints).toHaveLength(1);
+        });
     });
 });
