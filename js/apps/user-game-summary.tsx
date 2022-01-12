53636d2c8fffae59620827fb0a4a31a79ce305f7
Goyatuzo <yotagur@gmail.com>
2022-01-12 00:55:54 -0600

    Cleaner representation

--- js/apps/user-game-summary.tsx
+++ js/apps/user-game-summary.tsx
@@ -9,9 +9,19 @@ declare const userId: string;
 declare const gameName: string;
 
 render(<QueryClientProvider client={queryClient}>
-        <GameDetailPie detail={'gameDetail'} userId={userId} gameName={gameName} />
-        <GameDetailPie detail={'gameState'} userId={userId} gameName={gameName} />
-        <GameDetailPie detail={'smallAssetText'} userId={userId} gameName={gameName} />
-        <GameDetailPie detail={'largeAssetText'} userId={userId} gameName={gameName} />
+        <div className="ui four column stackable grid">
+                <div className="column">
+                        <GameDetailPie detail={'gameDetail'} userId={userId} gameName={gameName} />
+                </div>
+                <div className="column">
+                        <GameDetailPie detail={'gameState'} userId={userId} gameName={gameName} />
+                </div>
+                <div className="column">
+                        <GameDetailPie detail={'smallAssetText'} userId={userId} gameName={gameName} />\
+                </div>
+                <div className="column">
+                        <GameDetailPie detail={'largeAssetText'} userId={userId} gameName={gameName} />
+                </div>
+        </div>
     </QueryClientProvider>,
     document.getElementById("game-summary-graphs"))
